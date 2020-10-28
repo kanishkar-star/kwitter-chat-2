6aec6fc6c81096f24ed4ce022ef7ccca6182a1c0
kanishkar-star <71880783+kanishkar-star@users.noreply.github.com>
2020-10-28 16:16:09 +0100

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAEG5v_yiJeuiEIb9jn4bjosP5O4nlvx-g",
+    authDomain: "kwitter-let-s-chat.firebaseapp.com",
+    databaseURL: "https://kwitter-let-s-chat.firebaseio.com",
+    projectId: "kwitter-let-s-chat",
+    storageBucket: "kwitter-let-s-chat.appspot.com",
+    messagingSenderId: "163618188930",
+    appId: "1:163618188930:web:ffe6af781484fa8a5ace9b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
